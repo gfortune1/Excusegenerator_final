bbf64e388f7b02afb4e175f959c34c854fa076aa
gfortune1 <fgeee1@gmail.com>
2022-12-09 00:41:00 +0000

    escuseG

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the !");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  console.log(who[Math.floor(Math.random() * (who.length - 1))]);
+  document.innerHTML = "Hello world";
+  let excuse = document.querySelector("#excuse");
+  let who1 = who[Math.floor(Math.random() * (who.length - 1))];
+  let action1 = action[Math.floor(Math.random() * (action.length - 1))];
+  let what1 = what[Math.floor(Math.random() * (what.length - 1))];
+  let when1 = when[Math.floor(Math.random() * (when.length - 1))];
+  excuse.innerHTML = who1 + " " + action1 + " " + what1 + " " + when1;
 };
